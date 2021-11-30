085abc55d4288bb9aa484e1ce1320c9948765137
lucas <lucasportellaagu@gmail.com>
2021-11-30 12:33:26 -0300

    fix requirement request

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -5,6 +5,20 @@ class SimpleReport:
     current_date = datetime.now()
 
     @classmethod
+    def generate_products_object(cls, data):
+        # usar sort pq por algum motivo o primeiro for não começa
+        #  o loop pela ordem dos objetos
+        data.sort(key=lambda a: a["id"])
+        companies = []
+        for company in data:
+            if company["nome_da_empresa"] not in companies:
+                companies.append(company["nome_da_empresa"])
+        companies_products = {}
+        for company in companies:
+            companies_products[company] = 0
+        return companies_products
+
+    @classmethod
     def get_oldest_manuf_date(cls, data):
         manuf_dates = [date["data_de_fabricacao"] for date in data]
         manuf_dates.sort()
@@ -23,13 +37,22 @@ class SimpleReport:
 
     @classmethod
     def get_biggest_stock_company(cls, data):
-        for stock in data:
-            stock_age = datetime.strptime(
-                stock["data_de_validade"], "%Y-%m-%d"
-            ) - datetime.strptime(stock["data_de_fabricacao"], "%Y-%m-%d")
-            stock["stock_age"] = stock_age.days
-        data.sort(key=lambda a: a["stock_age"], reverse=True)
-        return data[0]["nome_da_empresa"]
+        companies_products = cls.generate_products_object(data)
+        for product in data:
+            companies_products[product["nome_da_empresa"]] += 1
+        sorted_companies = sorted(
+            companies_products.items(), key=lambda a: a[1], reverse=True
+        )
+        return sorted_companies[0][0]
+
+        # stock age
+        # for stock in data:
+        #     stock_age = datetime.strptime(
+        #         stock["data_de_validade"], "%Y-%m-%d"
+        #     ) - datetime.strptime(stock["data_de_fabricacao"], "%Y-%m-%d")
+        #     stock["stock_age"] = stock_age.days
+        # data.sort(key=lambda a: a["stock_age"], reverse=True)
+        # return data[0]["nome_da_empresa"]
 
     @classmethod
     def generate(cls, data):
@@ -50,3 +73,44 @@ class SimpleReport:
 # Empresa com maior quantidade de
 # produtos estocados: {cls.get_biggest_stock_company(data)}
 # """
+
+test_data = [
+    {
+        "id": 1,
+        "nome_do_produto": "CALENDULA OFFICINALIS FLOWERING TOP",
+        "nome_da_empresa": "Forces of Nature",
+        "data_de_fabricacao": "2020-07-04",
+        "data_de_validade": "2023-02-09",
+        "numero_de_serie": "FR48 2002 7680 97V4 W6FO LEBT 081",
+        "instrucoes_de_armazenamento": "in blandit ultrices enim",
+    },
+    {
+        "id": 2,
+        "nome_do_produto": "sodium ferric gluconate complex",
+        "nome_da_empresa": "sanofi-aventis U.S. LLC",
+        "data_de_fabricacao": "2020-05-31",
+        "data_de_validade": "2023-01-17",
+        "numero_de_serie": "SE95 2662 8860 5529 8299 2861",
+        "instrucoes_de_armazenamento": "duis bibendum morbi",
+    },
+    {
+        "id": 3,
+        "nome_do_produto": "Dexamethasone Sodium Phosphate",
+        "nome_da_empresa": "sanofi-aventis U.S. LLC",
+        "data_de_fabricacao": "2019-09-13",
+        "data_de_validade": "2023-02-13",
+        "numero_de_serie": "BA52 2034 8595 7904 7131",
+        "instrucoes_de_armazenamento": "morbi quis tortor id",
+    },
+    {
+        "id": 4,
+        "nome_do_produto": "Uricum acidum, Benzoicum acidum",
+        "nome_da_empresa": "Newton Laboratories, Inc.",
+        "data_de_fabricacao": "2019-11-08",
+        "data_de_validade": "2019-11-25",
+        "numero_de_serie": "FR38 9203 3060 400T QQ8B HHS0 Q46",
+        "instrucoes_de_armazenamento": "velit eu est congue elementum",
+    },
+]
+
+print(SimpleReport.generate(test_data))
