9cd43e79f3eda8323dd0d0d5c4ee69bbfd3d16eb
lucas <lucasportellaagu@gmail.com>
2021-12-01 14:09:22 -0300

    req 8 done - finish

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,22 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return sys.stderr.write("Verifique os argumentos\n")
+
+    parsers = {
+        "csv": CsvImporter,
+        "json": JsonImporter,
+        "xml": XmlImporter,
+    }
+
+    path = sys.argv[1]
+    report_type = sys.argv[2]
+    file_extension = path.split(".")[-1]
+    importer = InventoryRefactor(parsers[file_extension])
+    print(importer.import_data(path, report_type), end='')
